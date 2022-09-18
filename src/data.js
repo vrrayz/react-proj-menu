96289701d26486b4a98fd95dfcf80fb0d87143b1
Victor Okonkwo <emmyvic98@gmail.com>
2022-09-18 08:35:16 +0100

    setup data for project

--- src/data.js
+++ src/data.js
@@ -1,1 +1,84 @@
-export const menuOptions = ['all', 'breakfast', 'lunch', 'shakes']
+export const menuOptions = ["all", "breakfast", "lunch", "shakes"];
+export const menuItems = [
+  {
+    id: 1,
+    option: "",
+    name: "Buttermilk Pancakes",
+    img: "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg",
+    price: "15",
+    description:
+      "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
+  },
+  {
+    id: 2,
+    option: "",
+    name: "Diner Double",
+    img: "https://images.unsplash.com/photo-1460306855393-0410f61241c7",
+    price: "15",
+    description:
+      "vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats",
+  },
+  {
+    id: 3,
+    option: "",
+    name: "Godzilla Milkshake",
+    img: "https://images.pexels.com/photos/2638026/pexels-photo-2638026.jpeg",
+    price: "15",
+    description:
+      "ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.",
+  },
+  {
+    id: 4,
+    option: "",
+    name: "Country Delight",
+    img: "https://images.pexels.com/photos/103124/pexels-photo-103124.jpeg",
+    price: "15",
+    description:
+      "Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut,",
+  },
+  {
+    id: 5,
+    option: "",
+    name: "Egg Attack",
+    img: "https://images.pexels.com/photos/1251198/pexels-photo-1251198.jpeg",
+    price: "15",
+    description:
+      "franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up",
+  },
+  {
+    id: 6,
+    option: "",
+    name: "Oreo Dream",
+    img: "https://images.pexels.com/photos/3727250/pexels-photo-3727250.jpeg",
+    price: "15",
+    description:
+      "Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday",
+  },
+  {
+    id: 7,
+    option: "",
+    name: "Bacon Overflow",
+    img: "https://images.pexels.com/photos/139746/pexels-photo-139746.jpeg",
+    price: "15",
+    description:
+      "carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird",
+  },
+  {
+    id: 8,
+    option: "",
+    name: "American Classic",
+    img: "https://images.pexels.com/photos/551991/pexels-photo-551991.jpeg",
+    price: "15",
+    description:
+      "on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut",
+  },
+  {
+    id: 9,
+    option: "",
+    name: "Quarantine Buddy",
+    img: "https://www.onlineinterracialdatingsites.com/images/articleimages/interracial-dating-covid-19.jpg",
+    price: "15",
+    description:
+      "skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.",
+  },
+];
